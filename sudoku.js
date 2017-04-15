6c81c28f56affb5e009e1879ff0fc8e2a6db7bb4
xyvivian <xueying.ding@vanderbilt.edu>
2017-04-15 01:04:51 -0500

    printBoard

--- sudoku.js
+++ sudoku.js
@@ -2,7 +2,7 @@
 fs = require('fs');
 
 //this is a global variable
-let sudokuBoard=[];
+let sudokuBoard = [];
 
 
 function solve(row, col) {
@@ -26,44 +26,59 @@ function check(row, col, num) {
 }
 
 function printBoard() {
-
+    var row;
+    var col;
+    var line = "";
+    for (row = 0; row < 9; row++) {
+        for (col = 0; col < 9; col++) {
+            line = line + sudokuBoard[row][col] + " ";
+            if (col == 2 || col == 5) {
+                line = line + "| ";
+            }
+        }
+        console.log(line);
+        line = "";
+        if (row == 2 || row == 5) {
+            console.log("------+-------+------");
+        }
+    }
 }
 
 
-function readFile(fileName){
-   //opening the file
-    let contents = fs.readFileSync("./"+fileName).toString('utf-8');
+function readFile(fileName) {
+    //opening the file
+    let contents = fs.readFileSync("./" + fileName).toString('utf-8');
     let index = contents.indexOf('\n');
-    let rowNum=0;
+    let rowNum = 0;
 
     //reading the input file line by line
     while (index > -1) {
         let line = contents.substring(0, index);
         contents = contents.substring(index + 1);
         let arr = line.trim().split(/\s+/);
-        if(arr.length!=9){
+        if (arr.length != 9) {
             console.log("Each line of the input file must have 9 numbers");
             process.exit(1);
         }
-        sudokuBoard[rowNum]=arr;
+        sudokuBoard[rowNum] = arr;
         index = contents.indexOf('\n');
         rowNum++;
     }
-    if(rowNum!=9){
+    if (rowNum != 9) {
         console.log("The input file must contain 9 lines");
         process.exit(1);
     }
 }
 
-function main(){
+function main() {
     readFile("sudoku.txt");
     console.log("Here is the initial board");
     printBoard();
     let start = new Date();
-    if(solve(0,0)){
+    if (solve(0, 0)) {
         console.log("Here is the solution");
         printBoard();
-    }else{
+    } else {
         console.log("No solution");
     }
 
